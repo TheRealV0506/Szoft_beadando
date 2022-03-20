e2fbcc62ccaed19e627aaa1ab920d2910318d174
patrikmarodi <marodi.patrik@icloud.com>
2022-03-20 10:24:22 +0100

    added some css

--- src/main/java/hu/unideb/inf/MainApp.java
+++ src/main/java/hu/unideb/inf/MainApp.java
@@ -2,29 +2,16 @@ package hu.unideb.inf;
 
 import hu.unideb.inf.model.Model;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
-
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.io.PrintWriter;
 
 public class MainApp extends Application {
 
-    public Stage stage;
-
     @Override
     public void start(Stage stage) throws Exception {
-        Platform.setImplicitExit(false);
+        //Platform.setImplicitExit(false);
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/Login.fxml"));
         System.out.println(loader.hashCode());
         Scene loginScene = new Scene(loader.load());
@@ -35,7 +22,7 @@ public class MainApp extends Application {
     }
 
 
-    @FXML
+    /*@FXML
     public void changeScene(String fxmlstring) throws IOException
     {
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource(fxmlstring));
@@ -43,7 +30,7 @@ public class MainApp extends Application {
         stage.setScene(homepage);
         stage.show();
         stage.getScene().setRoot(loader.load());
-    }
+    }*/
 
     public static void main(String[] args) {
         launch(args);
